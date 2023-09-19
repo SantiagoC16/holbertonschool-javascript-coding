8923533b1a9a057703847b8f20d10f2a6a965f8d
SantiagoC16 <6317@holbertonstudents.com>
2023-09-19 08:33:08 -0700

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-fs.readFile(process.argv[2], "utf8", (error, data) => {
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
   if (error) {
     console.error(`${error}`);
     return;
