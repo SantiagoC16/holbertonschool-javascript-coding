22de076f0f7528b2e94746a8aa5dae96124139e9
SantiagoC16 <6317@holbertonstudents.com>
2023-09-20 19:17:05 -0700

    task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const fs = require('fs')
+const fs = require('fs');
 request(process.argv[2], (error, response, data) => {
   if (error) {
     console.error(error);
